e6adf18722d69b958845d723a1a977c742558fd1
AhmedAmgad28 <ahmed.amgad282001@yahoo.com>
2024-03-15 04:16:20 +0200

    get

--- Server/Server.js
+++ Server/Server.js
@@ -9,6 +9,10 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname + "/index.html"))
+})
+
 const expressServer = app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`)
 })
